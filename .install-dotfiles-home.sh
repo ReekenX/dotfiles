0623d7e1ae00b184fcbffc4a2d337c724ddbe441
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2014-03-06 07:52:56 +0200

    Improved installation script

--- .install-dotfiles-home.sh
+++ .install-dotfiles-home.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 cd
-git clone --bare https://github.com/ReekenX/dotfiles-home ~/.git
+
+# These "just for fun"
+echo "Checking operating system..."
+sleep 0.3
+echo "Checking file system..."
+sleep 0.4
+echo "Checking connected hardware..."
+sleep 0.4
+echo "Checking memory and disk space..."
+sleep 0.2
+echo "Downloading repository and required files..."
+git clone -quiet --bare https://github.com/ReekenX/dotfiles-home ~/.git
+echo "Performing required configuration for GIT..."
 git config core.bare false
+echo "Applying files to your home dir..."
 git checkout -f master
-git submodule init
-git submodule update
+echo "Downloading VIM plugins..."
+git submodule --quie init
+git submodule --quie update
 cd -
 echo "Done."
