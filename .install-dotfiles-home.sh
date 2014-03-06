c40360b69b549de6aba3e5b0384da41da5e166b1
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2014-03-06 07:59:17 +0200

    Fixed another mistypo in automatic installer

--- .install-dotfiles-home.sh
+++ .install-dotfiles-home.sh
@@ -12,7 +12,7 @@ sleep 0.4
 echo "Checking memory and disk space..."
 sleep 0.2
 echo "Downloading repository and required files..."
-git clone -quiet --bare https://github.com/ReekenX/dotfiles-home ~/.git
+git clone --quiet --bare https://github.com/ReekenX/dotfiles-home ~/.git
 echo "Performing required configuration for GIT..."
 git config core.bare false
 echo "Applying files to your home dir..."
