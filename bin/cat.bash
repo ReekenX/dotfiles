831d0c1de21ac8ea11658d325f7109b274f1b07e
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2016-02-01 09:42:26 +0200

    Add cat command which will use highlighting if available

---
+++ bin/cat.bash
@@ -0,0 +1,13 @@
+#!/bin/sh
+# if no file specified default to lolcat reading on stdin
+if [ $# -eq 0 ]; then
+    cat
+else
+    TYPE=$(pygmentize -N $1)
+
+    if [ $TYPE = "text" ]; then
+        cat $1
+    else
+        pygmentize $1
+    fi
+fi
