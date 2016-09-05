aa3b313e169c4491d81f99b18e416b2c7a00385d
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2016-09-05 06:31:51 +0300

    Change monitors script to set monitors properly with new video card

--- bin/4-monitors-setup.bash
+++ bin/4-monitors-setup.bash
@@ -2,12 +2,11 @@
 # Script to setup 4 monitors I have home with single command
 
 # Turn off monitors to prevent floating point errors
-xrandr --output HDMI-1 --off
-xrandr --output VGA-1 --off
-xrandr --output VGA-1-1 --off
+xrandr --output HDMI-0 --off
+xrandr --output VGA-0 --off
+xrandr --output DVI-1-1 --off
 
-# Restore monitors
 xrandr --output HDMI-1-1 --auto --pos 0x0
-xrandr --output HDMI-1 --auto --pos 1920x0
-xrandr --output VGA-1 --auto --pos 0x1080
-xrandr --output VGA-1-1 --auto --pos 1920x1080
+xrandr --output HDMI-0 --auto --pos 1920x0
+xrandr --output VGA-0 --auto --pos 0x1080
+xrandr --output DVI-1-1 --auto --pos 1920x1080
