bdf6ac9d164045d7fd1555e4e0f218e8e6fe2a86
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2016-11-11 06:55:48 +0200

    Add monitor fallback option into 4 monitors setup script

--- bin/4-monitors-setup.bash
+++ bin/4-monitors-setup.bash
@@ -4,9 +4,11 @@
 # Turn off monitors to prevent floating point errors
 xrandr --output HDMI-0 --off
 xrandr --output VGA-0 --off
+xrandr --output DVI-1-0 --off
 xrandr --output DVI-1-1 --off
 
 xrandr --output HDMI-1-1 --auto --pos 0x0
 xrandr --output HDMI-0 --auto --pos 1920x0
 xrandr --output VGA-0 --auto --pos 0x1080
+xrandr --output DVI-1-0 --auto --pos 1920x1080
 xrandr --output DVI-1-1 --auto --pos 1920x1080
