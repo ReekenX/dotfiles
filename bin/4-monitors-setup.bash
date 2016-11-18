ad94db1fbfbee6a83185facc8f19a9c855b88c15
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2016-11-18 13:30:54 +0200

    Update script to fix monitors

--- bin/4-monitors-setup.bash
+++ bin/4-monitors-setup.bash
@@ -2,13 +2,12 @@
 # Script to setup 4 monitors I have home with single command
 
 # Turn off monitors to prevent floating point errors
-xrandr --output HDMI-0 --off
-xrandr --output VGA-0 --off
-xrandr --output DVI-1-0 --off
-xrandr --output DVI-1-1 --off
+for monitor in "HDMI-1-1" "HDMI-0" "VGA-0" "DVI-1-0"
+do
+    xrandr --output $monitor --off
+done
 
 xrandr --output HDMI-1-1 --auto --pos 0x0
 xrandr --output HDMI-0 --auto --pos 1920x0
 xrandr --output VGA-0 --auto --pos 0x1080
 xrandr --output DVI-1-0 --auto --pos 1920x1080
-xrandr --output DVI-1-1 --auto --pos 1920x1080
