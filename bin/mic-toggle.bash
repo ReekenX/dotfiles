3c25e5ed00b995edbf85695ca55af2c7385f6940
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2017-04-22 11:15:18 +0300

    Remove bin folder - hardly reusable

--- bin/mic-toggle.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Toggle mic on-off with single command
-# Notify user with GUI message about it
-
-amixer set Capture toggle && amixer get Capture | \
-    grep '\[off\]' && \
-    notify-send -i microphone-sensitivity-muted "Microphone" "Microphone is switched OFF." || notify-send -i microphone-sensitivity-high "Microphone" "Microphone is switched ON, you can talk now."
--- bin/zoom-in-camera.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-guvcview -p ~/.camera.gpfl 2>&1 > /dev/null &
-sleep 1
-kill %1
