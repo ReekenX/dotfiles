a4e29b9ebb80fdc7b21fe87189a5f3c1ed76d3e0
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2022-04-01 11:24:34 +0300

    Restore VIM LSP jumping to definition and references watch

--- .vim/lsp_config_new.lua
+++ .vim/lsp_config_new.lua
@@ -1,6 +1,19 @@
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gj', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+end
+
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
@@ -69,6 +82,7 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 local servers = { 'solargraph', 'tsserver', 'pyright' }
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
     capabilities = capabilities
   }
 end
