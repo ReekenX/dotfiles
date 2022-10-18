b6e16a37bda34cd71c74944b9c30bf53cea5b929
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2022-10-18 16:27:04 +0300

    Fix package upgrade warning for VIM

--- .vim/lsp.lua
+++ .vim/lsp.lua
@@ -69,7 +69,7 @@ cmp.setup.cmdline('/', {
 })
 
 -- Setup LSP
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers = { 'solargraph', 'tsserver', 'pyright' }
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup {
