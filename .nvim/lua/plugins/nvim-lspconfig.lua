e105d3ed326737c1f5328f246713e0f9a668e70a
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2023-12-25 16:50:43 +0200

    Install LazyVIM to try

---
+++ .nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,16 @@
+return {
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        tsserver = {},
+        solargraph = {},
+      },
+    },
+  },
+}
