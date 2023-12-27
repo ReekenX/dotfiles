bbbcc4625f71dd709d547db10b5403ce059613c9
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2023-12-27 11:57:07 +0200

    Add custom keymap to copy filename to the clipboard

--- .nvim/lua/config/keymaps.lua
+++ .nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- Custom save keymap – ww in normal mode
+vim.keymap.set({ "n" }, "ww", "<cmd>w<cr><esc>", { desc = "Save file" })
+vim.keymap.del("n", "<C-s>")
+
+-- Copy filename to clipboard
+vim.keymap.set({ "n" }, "<leader>cf", '<cmd>:let @*=expand("%")<CR>', { desc = "Filename to clipboard" })
