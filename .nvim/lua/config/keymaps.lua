6d938ead130eb7a55f05a4a32ba29ed2ebfa9c18
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2024-01-27 10:54:08 +0200

    Add NVIM keymap to restart LSP server

--- .nvim/lua/config/keymaps.lua
+++ .nvim/lua/config/keymaps.lua
@@ -2,8 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
 -- Custom save keymap – ww in normal mode
-vim.keymap.set({ "n" }, "ww", "<cmd>w<cr><esc>", { desc = "Save file" })
+vim.keymap.set({ "n" }, "ww", "<cmd>w<cr><esc>", { desc = "Save File" })
 vim.keymap.del("n", "<C-s>")
 
 -- Copy filename to clipboard
-vim.keymap.set({ "n" }, "<leader>cf", '<cmd>:let @*=expand("%")<CR>', { desc = "Filename to clipboard" })
+vim.keymap.set({ "n" }, "<leader>cf", '<cmd>:let @*=expand("%")<CR>', { desc = "Filename to Clipboard" })
+
+-- Quick shortcut to restart LSP server
+vim.keymap.set({ "n" }, "<leader>R", "<cmd>:LspRestart<CR>", { desc = "Restart LSP Server" })
