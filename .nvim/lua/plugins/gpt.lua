0e9ede92b3ae4030de7ab6125ffa3b34a028ad6e
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2024-03-26 14:57:36 +0200

    Change GPT plugin for NVIM

--- .nvim/lua/plugins/gpt.lua
+++ .nvim/lua/plugins/gpt.lua
@@ -1,31 +1,10 @@
 return {
   {
-    "thmsmlr/gpt.nvim",
+    "CoderCookE/vim-chatgpt",
     config = function()
-      require("gpt").setup({
-        api_key = os.getenv("OPENAI_PRIVATE_API_KEY"),
-      })
-      opts = { silent = true, noremap = true }
-      vim.keymap.set("v", "<leader>gr", require("gpt").replace, {
-        silent = true,
-        noremap = true,
-        desc = "GPT Rewrite",
-      })
-      vim.keymap.set("v", "<leader>gp", require("gpt").visual_prompt, {
-        silent = false,
-        noremap = true,
-        desc = "GPT Prompt",
-      })
-      vim.keymap.set("n", "<leader>gp", require("gpt").prompt, {
-        silent = true,
-        noremap = true,
-        desc = "GPT Prompt",
-      })
-      vim.keymap.set("n", "<leader>gx", require("gpt").cancel, {
-        silent = true,
-        noremap = true,
-        desc = "GPT Cancel",
-      })
+      vim.g.chat_gpt_model = "gpt-4"
+      vim.g.chat_gpt_lang = "English"
+      vim.g.chat_gpt_max_tokens = 2000
     end,
   },
 }
