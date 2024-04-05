72003d21925bc66b519af27527377576803d4db7
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2024-04-05 07:33:44 +0300

    Automatically update NVim on Startup

--- .nvim/lua/config/autocmds.lua
+++ .nvim/lua/config/autocmds.lua
@@ -2,8 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.filetype.add({
-  extension = {
-    rabl = "ruby",
-  },
+-- Automatically update LazyVim and plugins on startup
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("autoupdate"),
+  callback = function()
+    require("lazy").update({
+      show = false,
+    })
+  end,
 })
