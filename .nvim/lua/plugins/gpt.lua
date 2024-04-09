73c023f3b3220c8307be04e7a8d8e76aec0b730b
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2024-04-09 09:41:29 +0300

    Fix highlighting issue in VIM GPT plugin

--- .nvim/lua/plugins/gpt.lua
+++ .nvim/lua/plugins/gpt.lua
@@ -6,8 +6,14 @@ return {
       vim.g.chat_gpt_lang = "English"
       vim.g.chat_gpt_max_tokens = 2000
       vim.g.chat_gpt_custom_prompts = {
-        ["ask"] = "I will ask you programming question. When answering be short and straightforward. If you will be returning code sample as an example - do not explain that code.",
+        ["ask"] = "When answering be short and straightforward. If you will be returning code sample as an example - do not explain that code.",
       }
+
+      -- Force markdown to see code highlights properly and hide weird characters
+      vim.api.nvim_create_autocmd("BufWinEnter", {
+        pattern = "gpt-persistent-session",
+        command = "set filetype=html isprint=1-255",
+      })
     end,
   },
 }
