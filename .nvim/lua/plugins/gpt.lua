66d597f3a66d20368967feaf66258998a65cf71e
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2024-04-09 09:30:58 +0300

    For VIM GPG plugin use custom prompt intro to improve response

--- .nvim/lua/plugins/gpt.lua
+++ .nvim/lua/plugins/gpt.lua
@@ -1,10 +1,13 @@
 return {
   {
     "CoderCookE/vim-chatgpt",
-    config = function()
+    init = function()
       vim.g.chat_gpt_model = "gpt-4"
       vim.g.chat_gpt_lang = "English"
       vim.g.chat_gpt_max_tokens = 2000
+      vim.g.chat_gpt_custom_prompts = {
+        ["ask"] = "I will ask you programming question. When answering be short and straightforward. If you will be returning code sample as an example - do not explain that code.",
+      }
     end,
   },
 }
