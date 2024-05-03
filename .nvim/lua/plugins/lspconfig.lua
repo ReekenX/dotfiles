5ebb116b633ce8544339c1e0339b354bf177e57d
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2024-05-03 10:40:35 +0300

    Enable inlay hints for all languages

--- .nvim/lua/plugins/lspconfig.lua
+++ .nvim/lua/plugins/lspconfig.lua
@@ -4,11 +4,8 @@ return {
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-        tsserver = {},
-        solargraph = {},
+      inlay_hints = {
+        enabled = true,
       },
     },
   },
