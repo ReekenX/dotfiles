7423df2b6ddfc7b979969ebd30f5dae6a3448259
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2026-03-26 16:25:59 +0200

    Always open nvim-tree editor when starting vim

--- .vim/lsp.lua
+++ .vim/lsp.lua
@@ -58,6 +58,14 @@ require("nvim-tree").setup({
   }
 })
 
+-- Auto-open nvim-tree on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("nvim-tree.api").tree.open()
+    vim.cmd("wincmd p")
+  end
+})
+
 require('gitsigns').setup {
   signs = {
     add          = { text = '┃' },
