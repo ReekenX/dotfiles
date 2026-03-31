75aa1e67c5ed5ae6f4d3834b3b85a1eff214fece
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2026-03-31 18:48:22 +0300

    Disable nvim markdown visual shifting

--- .nvim/lua/config/autocmds.lua
+++ .nvim/lua/config/autocmds.lua
@@ -7,6 +7,14 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Disable concealing in markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown", "markdown.mdx" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
 -- Auto save on focus lost
 vim.api.nvim_create_autocmd({ "FocusLost" }, {
   callback = function()
--- .nvim/lua/plugins/markdown.lua
+++ .nvim/lua/plugins/markdown.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = false,
+  },
+  {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
