bc531c124420d353895218124d8c8af391a96631
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2026-03-31 18:20:50 +0300

    Autosave in nvim

--- .nvim/lua/config/autocmds.lua
+++ .nvim/lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto save on focus lost
+vim.api.nvim_create_autocmd({ "FocusLost" }, {
+  callback = function()
+    vim.cmd("silent! wall")
+  end,
+})
