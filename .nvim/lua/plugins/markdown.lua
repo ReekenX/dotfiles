dede7bde33b70e4a872398a991ca4591dae96a17
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2026-04-06 07:56:09 +0300

    Disable markdown linter in nvim

--- .nvim/lua/plugins/markdown.lua
+++ .nvim/lua/plugins/markdown.lua
@@ -1,23 +1,29 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    enabled = false,
-  },
-  {
-    "mfussenegger/nvim-lint",
+    "neovim/nvim-lspconfig",
     opts = {
-      linters_by_ft = {
-        markdown = {},
+      servers = {
+        marksman = {},
       },
     },
   },
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        ["markdown"] = { "prettier", "markdown-toc" },
-        ["markdown.mdx"] = { "prettier", "markdown-toc" },
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
       },
     },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
   },
 }
