902af7f1c3d5146afa98b1ba795788d14a340043
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2026-04-22 15:13:31 +0300

    Show hidden files in nvim-tree plugin

---
+++ .nvim/lua/plugins/snacks-explorer.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
